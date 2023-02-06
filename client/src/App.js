89775245ad0cff3c37c7cc00fc68b912d65e0f27
Arindam Kundu <arindamkundu1091@gmail.com>
2023-02-06 09:54:22 +0530

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+// import TernaryOperator from './Cond1/TernaryOperator';
+
+//If-Statement
+// import DemoAuth from './Cond1/DemoAuth';
+// import IfStatement from './Cond1/IfStatement';
+
+
 function App() {
 
   return (
     <>
+      {/* <IfStatement isGreeting={true}/> */}
+      {/* <DemoAuth isLoggedin={true}/> */}
 
+      {/* <TernaryOperator isLoggedin={true}/> */}
+
+      
     </>
   );
 }
