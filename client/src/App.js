a68e5485f937caea34a67df2d5ebc8d1bfa1999d
Arindam Kundu <arindamkundu1091@gmail.com>
2023-02-06 09:10:52 +0530

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
-// import LYShouldComponentUpdate from './ClassComponentAll/ShouldComponentUpdate/LYShouldComponentUpdate';
-// import LYComponentDidMount from './ClassComponentAll/ComponentDidMount/LYComponentDidMount';
-// import LifeCycleDerivedState from './ClassComponentAll/getDrivedStateFromProps/LifeCycleDerivedState';
-// import LifeCycleConstructor from './ClassComponentAll/Constructor/LifeCycleConstructor';
-// import BasicLifeCycleComponent from './ClassComponentAll/LifeCycle/BasicLifeCycleComponent';
-// import AdvanceLifeCycleComponent from './ClassComponentAll/LifeCycle/AdvanceLifeCycleComponent';
-
-
 
 function App() {
 
   return (
     <>
-      {/* <BasicLifeCycleComponent /> */}
-      {/* <AdvanceLifeCycleComponent /> */}
-      {/* <LifeCycleConstructor /> */}
-      {/* <LifeCycleDerivedState col='yellow'/> */}
-      {/* <LYComponentDidMount /> */}
-      {/* <LYShouldComponentUpdate /> */}
+
     </>
   );
 }
