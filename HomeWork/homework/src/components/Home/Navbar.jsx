1c4e48b5568eb680edb897ea54d8ef0062a08d07
Arindam Kundu <arindamkundu1091@gmail.com>
2023-02-07 20:11:19 +0530

    ...

--- HomeWork/homework/src/components/Home/Navbar.jsx
+++ HomeWork/homework/src/components/Home/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar(props) {
         <div>
             <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">Navbar</a>
+                <a className="navbar-brand" href="/">Page Title</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
