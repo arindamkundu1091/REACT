236d8b4b070765a54d6d8698014e16313e2ab700
Arindam Kundu <arindamkundu1091@gmail.com>
2023-02-09 09:40:31 +0530

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import DataList from './Problem 2/DataList';
-import BookData from './Problem 1/BookData';
 
 function App() {
 
   return (
     <>
-      <BookData />
-      <DataList />
+      
     </>
   );
 }
