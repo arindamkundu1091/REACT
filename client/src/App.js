4ebee80df09c452719ce8a4836cf339b6580afeb
Arindam Kundu <arindamkundu1091@gmail.com>
2023-02-09 11:02:53 +0530

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Problem2 from './Hooks/UseStateHooks/Problem2';
+// import Problem2 from './Hooks/UseStateHooks/Problem2';
 // import Problem1 from './Hooks/UseStateHooks/Problem1';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       {/* <Problem1 /> */}
-      <Problem2 />
+      {/* <Problem2 /> */}
     </>
   );
 }
