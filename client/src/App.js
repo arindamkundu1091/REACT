6ad9015f1b5ed92ce31c830eaf9bb8178595b0d9
Arindam Kundu <arindamkundu1091@gmail.com>
2023-02-11 11:33:27 +0530

    ...

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,9 @@
 import React from 'react';
-// import CustomHooksImplementatin from './Hooks/UseEffectHooks/FetchData/CustomHooksImplementatin';
-// import Example3 from './Hooks/UseEffectHooks/Example3';
-// import Problem1 from './Hooks/UseEffectHooks/Problem1';
-// import Example2 from './Hooks/UseEffectHooks/Example2';
-// import Example1 from './Hooks/UseEffectHooks/Example1';
 
 function App() {
 
   return (
     <>
-      {/* <Example1 /> */}
-      {/* <Example2 /> */}
-      {/* <Problem1 /> */}
-      {/* <Example3 /> */}
-      {/* <CustomHooksImplementatin /> */}
     </>
   );
 }
