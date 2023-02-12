628423dcb3fc2c9a05dc4dad7ffc9bfbfcf6a621
Arindam Kundu <arindamkundu1091@gmail.com>
2023-02-12 10:25:44 +0530

    ...

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -26,7 +26,6 @@ function Layout() {
                     <li> <NavLink to='/'>Home</NavLink> </li>
                     <li><NavLink to='/about'>About</NavLink></li>
                     <li><NavLink to='/contact'>Contact</NavLink></li>
-                    <li><NavLink to='/gallary'>Gallary</NavLink></li>
                 </ul>
             </nav>
             <div>
