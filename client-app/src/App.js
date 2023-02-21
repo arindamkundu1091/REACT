3ad2694747ad20e5ce667567864e2d453c173f64
Arindam Kundu <arindamkundu1091@gmail.com>
2023-02-21 18:50:48 +0530

    ...

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,19 +1,9 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import QuizPage from "./components/UI/Card/QuizPage";
-import StartQuiz from "./components/UI/Button/StartQuiz";
-import Result from "./components/UI/Input/Result";
 
 function App() {
   return (
     <>
-      <h2 className="heading">Quizz App</h2>
-      <Routes>
-        <Route exact path="/" element={<StartQuiz />}/>
-        <Route path="/quizpage" element={<QuizPage />} />
-        <Route path="/result" element={<Result />} />
-      </Routes>
     </>
   );
 }
