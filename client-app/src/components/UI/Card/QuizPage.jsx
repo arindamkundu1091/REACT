66ed990739f10fdc8405325a61014987174bc80a
Arindam Kundu <arindamkundu1091@gmail.com>
2023-02-21 09:54:26 +0530

    ...

--- client-app/src/components/UI/Card/QuizPage.jsx
+++ client-app/src/components/UI/Card/QuizPage.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
+import React ,{useEffect} from 'react'
 import ShowResult from '../Button/ShowResult';
+import Result from '../Input/Result';
 
 export default function QuizPage() {
 
-    const [state, setState] = React.useState(true);
+    const [state, setState] = React.useState(false);
+    const [count, setCount] = React.useState(0);
+    const [score, setScore] = React.useState(1);
+
+    const counter = () => {
+        setCount(count + 1);
+    }
+    
+    useEffect(() => {
+        if (count === 5) {
+            setState(true);
+        }
+    }, [setState, count]);
+    
+    const scoreCoutner = () => {
+        setScore(score + 1);
+        console.log(score);
+        counter();
+        <Result score={score} />
+    }
 
     const show = () => {
         if (state) {
             return (
                 <ShowResult />
-            )
-        } else {
+            );
         }
-    }
+    };
+
 
     return (
         <React.Fragment>
@@ -20,46 +40,46 @@ export default function QuizPage() {
                 <div className="main">
                     <div className="card card1">
                         <h3 className="question">Who is the father of our nation ?</h3>
-                        <button className="bigbtn">
+                        <button className="bigbtn" onClick={scoreCoutner}>
                             Mahatma Gandhi
                         </button>
-                        <button className="bigbtn">
+                        <button className="bigbtn" onClick={counter}>
                             Jawaharlal Nehru
                         </button>
-                        <button className="bigbtn"> 
+                        <button className="bigbtn" onClick={counter}> 
                             Donal Trump
                         </button>
-                        <button className="bigbtn"> 
+                        <button className="bigbtn" onClick={counter}> 
                             Barrack Obama
                         </button>
                     </div>
                     <div className="card card1">
                         <h3 className="question">What color is are the leaves ?</h3>
-                        <button className="btn">Blue</button>
-                        <button className="btn">Red</button>
-                        <button className="btn">Yellow</button>
-                        <button className="btn">Green</button>
+                        <button className="btn" onClick={counter}>Blue</button>
+                        <button className="btn" onClick={counter}>Red</button>
+                        <button className="btn" onClick={counter}>Yellow</button>
+                        <button className="btn" onClick={scoreCoutner}>Green</button>
                     </div>
                     <div className="card card2">
                         <h3 className="question">What color is the sky ?</h3>
-                        <button className="btn">Blue</button>
-                        <button className="btn">Red</button>
-                        <button className="btn">Yellow</button>
-                        <button className="btn">Green</button>
+                        <button className="btn" onClick={scoreCoutner}>Blue</button>
+                        <button className="btn" onClick={counter}>Red</button>
+                        <button className="btn" onClick={counter}>Yellow</button>
+                        <button className="btn" onClick={counter}>Green</button>
                     </div>
                     <div className="card card2">
                         <h3 className="question">What color is the sky ?</h3>
-                        <button className="btn">Blue</button>
-                        <button className="btn">Red</button>
-                        <button className="btn">Yellow</button>
-                        <button className="btn">Green</button>
+                        <button className="btn" onClick={scoreCoutner}>Blue</button>
+                        <button className="btn" onClick={counter}>Red</button>
+                        <button className="btn" onClick={counter}>Yellow</button>
+                        <button className="btn" onClick={counter}>Green</button>
                     </div>
                     <div className="card card2">
                         <h3 className="question">What color is the fire ?</h3>
-                        <button className="btn">Blue</button>
-                        <button className="btn">Red</button>
-                        <button className="btn">Yellow</button>
-                        <button className="btn">Green</button>
+                        <button className="btn" onClick={counter}>Blue</button>
+                        <button className="btn" onClick={counter}>Red</button>
+                        <button className="btn" onClick={scoreCoutner}>Yellow</button>
+                        <button className="btn" onClick={counter}>Green</button>
                     </div>
                 </div>
             </div>
--- client-app/src/components/UI/Input/Result.jsx
+++ client-app/src/components/UI/Input/Result.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import StartQuiz from '../Button/StartQuiz';
 
-export default function Result() {
+export default function Result(props) {
+    console.log(props.score);
     return (
         <>
             <StartQuiz/>
             <div className='center'>
                 <div style={{width: "650px", textAlign:"center", backgroundColor: "rgb(235, 156, 125)"}}>
-                    <p>You have answered correctly</p>
+                    <p>You scored {props.score} out of 5</p>
                 </div>
             </div>
         </>
